ea4fd03523902f27b2d6bd8067379a13f6854739
Stking19 <okolistephen43@gmail.com>
2025-04-05 13:44:06 -0700

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import PrivateRoute from './routes/PrivateRoute'
 import Error from './components/error/Error'
 import LandingPage from './pages/LandingPage'
 import Home from './pages/Home'
+import Login from './auth/login/Login'
+import SignUp from './auth/register/SignUp'
+import PasswordReset from './auth/resetPassword/PasswordReset'
+import ForgotPassword from './auth/forgotPassword/ForgotPassword'
+import Verify from './auth/verify/Verify'
+import Listing from './pages/Arinze/listing/Listing'
+import PropertyDetails from './pages/Arinze/propertydetails/PropertyDetails'
+import Payment from './pages/Joshua/payment/Payment'
+import TermsCondition from './pages/Joshua/T&Cs/TermsCondition'
 
 const App = () => {
   const router = createBrowserRouter([
@@ -15,12 +24,57 @@ const App = () => {
           path: '/',
           element: <Home />
         },
+
+        {
+          path: '/propertydetails/:id',
+          element: <PropertyDetails />,
+        },
+
+        {
+          path: '/listings',
+          element: <Listing />,
+        },
+
+        {
+          path: '/T&Cs',
+          element: <TermsCondition />
+        },
       ]
+    },
+
+    {
+      path: "/sign-in",
+      element: <Login />
+    },
+
+    {
+      path: "/register",
+      element: <SignUp />
+    },
+    
+    {
+      path: "/reset-password",
+      element: <PasswordReset />
+    },
+
+    {
+      path: "/forgot-password",
+      element: <ForgotPassword />
+    },
+
+    {
+      path: "/verify",
+      element: <Verify />
     },
 
     {
       path: '/private',
       element: <PrivateRoute />
+    },
+
+    {
+      path: '/payment',
+      element: <Payment />
     },
 
     {
