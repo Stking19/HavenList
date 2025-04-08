b5b48111617a35b62e1d0eae55bfae73265e18ce
PopeFrancis003 <onyekwereobinna1@gmail.com>
2025-04-08 13:32:25 +0100

    listing page and product detail page

--- src/pages/Arinze/listing/Listing.jsx
+++ src/pages/Arinze/listing/Listing.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './listing.css'
+import ListingCard from '../../../components/listingCard/ListingCard'
 
 const Listing = () => {
   return (
-    <div>Listing</div>
+    <div className='listingMain'>
+  <div className='listingHeroWrapper'>
+     <div className='listingHero'>
+        <h2>Property Listings</h2>
+        <p>"Explore the Best Properties in Your Area</p>
+     </div>
+  </div>
+  <div className='listingCardHolder'>
+      <div className='listingCardWraper'>
+      <ListingCard/>
+      <ListingCard/>
+      <ListingCard/>
+      <ListingCard/>
+      <ListingCard/>
+      </div>
+  </div>
+    </div>
   )
 }
 
