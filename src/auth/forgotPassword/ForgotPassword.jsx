f1b857c380425b36b55d645792aa167f578eed3c
odijosh <joshuaonuoha674@gmail.com>
2025-04-08 02:53:33 -0700

    first commit

--- src/auth/forgotPassword/ForgotPassword.jsx
+++ src/auth/forgotPassword/ForgotPassword.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import "./forgotPassword.css"
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
+    <div>
+      <div className="mainbody">
+      <div className="pageheader">
+        <h1>Forgot Password</h1>
+      </div>
+
+      <div className="forgotpasswordbody">
+        <div className="forgotpasswordtext">
+        <div className="textwrapper">
+          <h1>Enter the new password</h1>
+          <p>Your new password must be different</p>
+          <p style={{marginLeft: "70px"}}>from previously used password</p>
+          </div>
+        </div>
+
+        <div className="formdetailwrap">
+          {/* <form> */}
+          <div className="forgotpassworddetailwrapper">
+            <h2 style={{ color: "#00bcd4",fontSize: "30px" }}>Password</h2>
+            <input
+              className="resetinputcont"
+              type="text"
+              placeholder="Create password"
+            />
+          </div>
+
+          <div className="forgotpassworddetailwrapper">
+            <h2 style={{ color: "#00bcd4", fontSize: "30px" }}>Confirm Password</h2>
+            <input
+              className="resetinputcont"
+              type="text"
+              placeholder="Confirm password"
+            />
+          </div>
+             
+             <div className='buttoncont1'>
+          <button className="continuebtn">Continue</button>
+          </div>
+
+          {/* </form> */}
+        </div>
+      </div>
+    </div>
+      
+
+    </div>
   )
 }
 
