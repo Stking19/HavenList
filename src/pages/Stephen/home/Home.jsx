b71f79c6ba4ed154fb31b3f6fa3421361f4d1846
Stking19 <okolistephen43@gmail.com>
2025-04-08 03:41:14 -0700

    finished header footer search bar and still working on home page

--- src/pages/Stephen/home/Home.jsx
+++ src/pages/Stephen/home/Home.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
+import SearchBar from "../../../components/searchbar/SearchBar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="heroPage">
+        <h1>FIND YOUR PERFECT HOME</h1>
+        <h3>Verified Listing. Transparent Prices. Happy Homes.</h3>
+        <SearchBar />
+      </div>
+      <div className="aboutIc">
+        <div className="partL"></div>
+        <div className="partL"></div>
+        <div className="partL"></div>
+        <div className="partL"></div>
+        <div className="partL"></div>
+        <div className="partL"></div>
+      </div>
+      <div className="properties">
+        <div className="upHouse">
+          <div className="leftHouse">
+            <div className="houseDet">
+              <div className="Himg"></div>
+              <div className="Hdetails"></div>
+            </div>
+            <div className="apart"></div>
+          </div>
+          <div className="rightHouse">
+          <div className="houseDet">
+            <div className="Himg"></div>
+            <div className="Hdetails"></div>
+          </div>
+          <div className="apart"></div>
+          </div>
+        </div>
+        <div className="downHouse">
+          <div className="leftHouse"></div>
+          <div className="rightHouse"></div>
+        </div>
+      </div>
+      <div className="more">
+        <h1>More Property Listings</h1>
+        <div className="our">
+          <div className="houseImg"></div>
+          <div className="mission"></div>
+        </div>
+      </div>
+      <div className="numbers">
 
-export default Home
+      </div>
+      <div className="coming">
+        <div className="ekiti"></div>
+        <div className="osun"></div>
+        <div className="ondo"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
