1191e3b9d69a16f0dcb31a8cfc34896f88da00e5
Stking19 <okolistephen43@gmail.com>
2025-04-10 13:07:12 -0700

    fixed vercel error

--- src/auth/forgotPassword/ForgotPassword.jsx
+++ src/auth/forgotPassword/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./forgotpassword.css";
+import './forgotPassword.css'
 
 const ForgotPassword = () => {
   return (
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,6 @@ import Loadscreen from '../loadscreen/Loadscreen'
 import Header from '../components/header/Header'
 import { Outlet } from 'react-router'
 import Footer from '../components/footer/Footer'
-import AboutUsPage from './Joshua/AboutUsPage/AboutUsPage'
 
 const LandingPage = () => {
     const [isLoading, setIsLoading] = useState(true)
@@ -17,8 +16,7 @@ const LandingPage = () => {
   return (
     isLoading ? <Loadscreen /> : <div>
         <Header />
-        <AboutUsPage />
-        {/* <Outlet /> */}
+        <Outlet />
         <Footer />
     </div>
   )
