4e581ff16b729d8ccef247225b519bcea488dd94
Stking19 <okolistephen43@gmail.com>
2025-04-11 06:21:25 -0700

    presentation

--- src/pages/Arinze/LandLordListing.jsx
+++ src/pages/Arinze/LandLordListing.jsx
@@ -5,13 +5,9 @@ import ListingCard from '../../components/listingCard/ListingCard'
 const LandLordListing = () => {
   return (
     <div className='landLordListingMain'>
-      <div className='asideDashBord'>
-
-      </div>
       <div className='landLordMainScreen'>
         <span className='textHeader'>
             <h3>My Listings</h3>
-            <h3>Upload Property</h3>
         </span>
         <div className='landlordListingHolder'>
            <ListingCard/>
