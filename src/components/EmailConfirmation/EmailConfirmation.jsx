2bf1290ff3546fd2f2b122c00e7440ae6c7fa373
Stking19 <okolistephen43@gmail.com>
2025-04-15 18:05:02 -0700

    vercel configs

--- src/components/EmailConfirmation/EmailConfirmation.jsx
+++ src/components/EmailConfirmation/EmailConfirmation.jsx
@@ -15,7 +15,7 @@ const EmailConfirmation =() => {
   useEffect(() => {
     const handleVerify = async () => {
       try {
-        const response = await axios.get(`${API_URL}landlord-verify/${token}`);
+        const response = await axios.get(`${API_URL}emailStatus/${token}`);
         console.log(response);
         navigate("/sign-in/landlord");
       }catch (error) {
