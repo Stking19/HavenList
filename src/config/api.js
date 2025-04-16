8b4877d3075e3ab12b5a1e5439ff227e58ddced5
arinzeMino <arinzemino6@gmail.com>
2025-04-16 05:30:39 -0700

    fixed

--- src/config/api.js
+++ src/config/api.js
@@ -89,5 +89,15 @@ export const forgetPassword = async (email) => {
   }
 };
 
+export const getLandLordListing = async ()=>{
+  try {
+    const res = await axios.get(``)
+    console.log(res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 export default api;
+ 
